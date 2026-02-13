2075e806173530f9041e90bc29c1b4030e4714a6
samaah09 <k256510@nu.edu.pk>
2026-02-13 11:49:15 +0500

    task1.c

---
+++ task1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("enter your attendance: ",a);
+scanf("%d",&a);
+if(a>75){
+printf("you're selected.");}
+else{
+printf("not selected.");
+
+}
+return 0;
+}
